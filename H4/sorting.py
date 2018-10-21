2af7719af21692d65c02ae97c1e87bb353982b8f
ysantur <ysantur@gmail.com>
2018-10-21 16:18:45 +0300

    Update sorting.py

--- H4/sorting.py
+++ H4/sorting.py
@@ -98,31 +98,6 @@ def merge_sort(list):
     return (start + end)
 
 
-
-def radixsort(list):
-    n = len(list)
-    modulus = 10
-    div = 1
-    while True:
-        # empty array, [[] for i in range(10)]
-        new_list = [[], [], [], [], [], [], [], [], [], []]
-        for value in list:
-            least_digit = value % modulus
-            least_digit //= div
-            new_list[least_digit].append(value)
-        modulus = modulus * 10
-        div = div * 10
-
-        if len(new_list[0]) == n:
-            return new_list[0]
-
-        list = []
-        rd_list_append = list.append
-        for x in new_list:
-            for y in x:
-                rd_list_append(y)
-
-
 def countingsort(list):
     max_val=max(list)
     m = max_val + 1

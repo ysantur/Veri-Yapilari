3cbf116eade13f78932e86a0816fcf77558bf04d
ysantur <ysantur@gmail.com>
2019-11-04 15:07:08 +0300

    Update liste2.py

--- H5/liste2.py
+++ H5/liste2.py
@@ -30,19 +30,17 @@ class Liste(object):
 
     def remove(self, node_value):
         current_node = self.head
- 
+        previous_node = None
         while current_node is not None:
             if current_node.data == node_value:
-                if current_node.prev is  None:
-                    self.head = current_node.next
-                    current_node.next.prev = None
-
+                # if this is the first node (head)
+                if previous_node is not None:
+                    previous_node.next = current_node.next
                 else:
-                    current_node.prev.next = current_node.next
-                    current_node.next.prev = current_node.prev
- 
+                    self.head = current_node.next
+             previous_node = current_node
             current_node = current_node.next
-        Liste.cnt-=1
+         Liste.cnt-=1
             
     def print(self):
         current_node = self.head
